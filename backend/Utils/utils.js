b2e66f7f3044576f33b5cc32544452a0f4ef5b1e
unknown <nivalditin89@gmail.com>
2021-05-18 00:08:37 -0400

    registro de user con login y registro de login

--- backend/Utils/utils.js
+++ backend/Utils/utils.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
 
 
 
@@ -10,3 +11,18 @@ export const generarToken = (user) =>{
         expiresIn:'1d'
     });
 }
+
+export const registrarLogin = async (userid,loginlogs) =>{
+    const dateobj = new Date();
+    const day = dateobj.getDay();
+    const month = dateobj.getMonth()+1;
+    const year = dateobj.getFullYear();
+    const hora = dateobj.getHours();
+    const minutos = dateobj.getMinutes();
+    const sec = dateobj.getSeconds();
+    const loginLog = `fecha: ${day}/${month}/${year} hora: ${hora}:${minutos}:${sec}`;
+
+    
+    const userFound = await User.findByIdAndUpdate(userid,{logslogin:[...loginlogs,loginLog]},{useFindAndModify:false});
+    
+};
--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import * as utils from '../Utils/utils.js';
+import bcrypt from 'bcryptjs';
 
 
 
@@ -7,16 +8,47 @@ export const updateUser = (req,res)=>{
     res.send('atualiza user');
 }
 
-export const registrarUser =(req,res)=>{
-    res.send('registra user');
+export const registrarUser =async (req,res)=>{
+    const {nombre,apellidos,usuario,correo,password} = req.body;
+    
+    const checkUser = await User.findOne({correo:req.body.correo},{useFindAndModify:false});
+    if(checkUser){
+        res.status(400).send({message:'Este user ya existe!'});
+    }else{
+        const logslogin = [];
+        const newUser = new User({
+            nombre,
+            apellidos,
+            usuario,
+            correo,
+            password:bcrypt.hashSync(password,8),
+            logslogin,
+            faction:"",
+            clase:""
+        });             
+        const user = await newUser.save();  
+        if(user){
+            utils.registrarLogin(user._id,user.logslogin);
+            res.send({
+                _id:user._id,
+                usuario:user.usuario,
+                faction:user.faction,
+                clase:user.clase,
+                correo:user.correo,
+                token:utils.generarToken(user)
+            })
+            } else {
+                res.status(400).json({message:'Datos inválidos!'});
+            }   
+    }   
 }
 
 export const login = async (req,res)=>{
     const user = await User.findOne({$or:[{correo:req.body.correo},{usuario:req.body.usuario}]},{useFindAndModify:false});   
     
     if(user){        
-        if(req.body.password===user.password){
-            
+        if(bcrypt.compareSync(req.body.password,user.password)){
+          utils.registrarLogin(user._id,user.logslogin); 
             res.send({
                 _id:user._id,
                 token:utils.generarToken(user)
