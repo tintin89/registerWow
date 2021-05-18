9427a05310e995cfa8a4b76f4d76a50e24461601
unknown <nivalditin89@gmail.com>
2021-05-18 01:38:26 -0400

    implementacion del flujo de actions para el login y registro

---
+++ src/store/actions/userAction.js
@@ -0,0 +1,51 @@
+import * as actionTypes from './actionsType';
+import Axios from 'axios';
+
+
+export const updateUserInfo = (userInfo) =>{
+    return {
+        type:actionTypes.UPDATE_USER_INFO,
+        usuario:userInfo
+    }
+}
+
+
+
+export const updateErrorM = (m)=>{
+    return {
+        type:actionTypes.UPDATE_ERROR_MENSAJE,
+        mensaje:m
+    }
+}
+
+export const updateCargando = (l)=>{
+    return {
+        type:actionTypes.UPDATE_CARGANDO,
+        loading:l
+    }
+}
+
+export const loginWow = (usuario,password) =>{
+    return dispatch => {
+            dispatch(updateCargando(true));    
+            Axios.post('http://localhost:5000/api/login',{usuario,password})
+            .then(response=>{
+                dispatch(updateUserInfo(response.data));
+                localStorage.setItem('userInfo',JSON.stringify(response.data));
+                dispatch(updateCargando(false));
+                dispatch(updateErrorM(""));
+            })
+            .catch(error=>{                
+                dispatch(updateCargando(false));
+                dispatch(updateErrorM(error.response.data.message));
+            })         
+    }
+}
+
+export const logoutWow =()=>{
+    return dispatch =>{
+        localStorage.removeItem('userInfo');
+        dispatch(updateUserInfo(""));
+        
+    }
+}
--- src/store/reducer/globalState.js
+++ src/store/reducer/globalState.js
@@ -1,4 +1,4 @@
-
+import * as actionTypes from '../actions/actionsType';
 
 
 
@@ -9,8 +9,24 @@ const initialState = {
 }
 
 const globalState = (state=initialState,action)=>{
-    switch (action) {
-        
+    switch (action.type) {
+        case actionTypes.UPDATE_USER_INFO:
+            return {
+                ...state,
+                userInfo:{...action.usuario}
+            }
+
+        case actionTypes.UPDATE_ERROR_MENSAJE:
+           return {
+               ...state,
+               errorMensaje:action.mensaje
+              }       
+              
+              case actionTypes.UPDATE_CARGANDO:
+                return {
+                    ...state,
+                    cargando:action.loading
+                   }   
         default:
             return state
     }
