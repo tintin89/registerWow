d5f47f0b53ac4e581945a802207a68ba1e484ac1
unknown <nivalditin89@gmail.com>
2021-05-18 02:22:39 -0400

    pagina de perfil de usuario

--- src/components/MyAccount/MyAccount.js
+++ src/components/MyAccount/MyAccount.js
@@ -1,11 +1,108 @@
-import React from 'react'
+import {useState} from 'react';
+import './MyAccount.css'
+import alianza from '../../Assets/alianza.png';
+import horda from '../../Assets/horda.png';
+import { Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {useSelector,useDispatch} from 'react-redux';
+import {logoutWow} from  "../../store/actions/userAction";
 
-function MyAccount() {
+import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
+import IconButton from '@material-ui/core/IconButton'
+import {wowclases} from '../../imgData';
+
+const mapState = (globalState) =>({
+  user:globalState.userInfo
+})
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  },
+}));
+
+
+function MyAccount({showLBar}) {
+  const dispatch = useDispatch();
+  
+  const {user} = useSelector(mapState);  
+  
+  
+  const classes = useStyles();
+ 
+  
+  
+   
+    const getLevel = () =>{
+      let count = 0;
+     for (var k in user.arreglo){
+      if (user.arreglo.hasOwnProperty(k)) ++count;
+     } 
+     return count;
+    }
+
+
     return (
-        <div>
-            My Account
+        <div className="myAccount">
+         
+      
+          <div style={{transform:showLBar && 'translateX(0)'}}  className="myAccount__lateralBar">
+          <Avatar alt="Nombre" 
+          src={
+            wowclases.find(c=>c?.clase === user?.clase)?.img
+          }
+           className={classes.large}/>
+         <div className="myAccount__profile">            
+              <div  className="myAccount__profile__campo">
+              <h3>Usuario</h3>
+              <span>{user?.usuario}</span>
+              </div>
+              <div  className="myAccount__profile__campo">
+              <h3>Clase</h3>
+              <span>{user?.clase}</span>
+              </div>
+              <div className="myAccount__profile__campo">
+              <h3>Facción</h3>
+              <span>{user?.faction}</span>
+              </div>
+              <div className="myAccount__profile__campo">
+              <h3>Nivel</h3>
+              <span>{getLevel()+1}</span>
+              </div>  
+              <div className="myAccount__profile__campo">
+              <h3>Correo</h3>
+              <span>{user?.correo}</span>
+              </div>   
+
+              <div className="logout">
+              <IconButton onClick={()=>dispatch(logoutWow())}>
+                <ExitToAppRoundedIcon/>
+              </IconButton>
+              </div>  
+          
+         </div>
+          
+          
+          </div>
+          <div className="myAccount__welcome">
+            <h1>Bienvenido a Azeroth</h1>
+            <img alt="logoFaction" src={user?.faction === "alianza" ? alianza : horda}/>
+          </div>
+
         </div>
     )
 }
 
 export default MyAccount
+
---
+++ src/imgData.js
@@ -0,0 +1,41 @@
+
+import warrior from './Assets/warrior.png';
+import mage from './Assets/mage.png';
+import druid from './Assets/druid.png';
+import pala from './Assets/pala.png';
+import hunter from './Assets/hunter.png';
+import dk from './Assets/dk.png';
+import dh from './Assets/demonhunter.png';
+import warlock from './Assets/warlock.png';
+import priest from './Assets/priest.png';
+import monk from './Assets/monk.png';
+import shaman from './Assets/shaman.png';
+import rogue from './Assets/rogue.png';
+
+export const wowclases = [
+    {clase:'mago',
+     img:mage},
+     {clase:'sacerdote',
+     img:priest},
+     {clase:'brujo',
+     img:warlock},
+     {clase:'picaro',
+     img:rogue},
+     {clase:'druida',
+     img:druid},
+     {clase:'cazador de demonios',
+     img:dh},
+     {clase:'chaman',
+     img:shaman},
+     {clase:'cazador',
+     img:hunter},
+     {clase:'guerrero',
+     img:warrior},
+     {clase:'caballero de la muerte',
+     img:dk},
+     {clase:'paladin',
+     img:pala},
+     {clase:'monje',
+     img:monk},
+    
+  ];
