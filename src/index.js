20757ad08a2858184eb35b83a7cf4193acc6226d
unknown <nivalditin89@gmail.com>
2021-05-18 01:25:41 -0400

    configuracion de redux para almacenar el estado global

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware,compose } from 'redux';
+import thunk from 'redux-thunk';
+import globalState from './store/reducer/globalState';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
+
+
+
+const store = createStore(globalState,composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 
 
 ReactDOM.render(  
-    <App />
-  ,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
-
-
---
+++ src/store/reducer/globalState.js
@@ -0,0 +1,19 @@
+
+
+
+
+const initialState = {
+    userInfo:"",
+    cargando:false,
+    errorMensaje:""
+}
+
+const globalState = (state=initialState,action)=>{
+    switch (action) {
+        
+        default:
+            return state
+    }
+}
+
+export default globalState;
