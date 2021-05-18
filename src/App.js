ffada583b7078a4253e3538191fe60550fc8e06d
unknown <nivalditin89@gmail.com>
2021-05-18 01:05:56 -0400

    configuracion de las visas con react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router";
+
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="app">
-     
+    
+    
+    <div className="app__container">
+    <BrowserRouter>  
+      <Switch>
+        <Route path="/login" render={()=>(
+          <Login/>
+        )}/>    
+        <Route path="/" render={()=>(      
+            <Home/>
+       )}/>      
+      </Switch>        
+      </BrowserRouter>
     </div>
+    
+       
+  </div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyAccount from '../components/MyAccount/MyAccount'
+
+
+
+function Home(props) {
+    return   (    
+       <MyAccount/>   
+      )
+}
+
+export default Home
